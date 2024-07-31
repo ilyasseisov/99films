54910c2f92f9a4e93f525182d051c95e5c260063
Ilyas Seisov <ilyasseisov@gmail.com>
2024-07-31 10:28:58 +0500

    add onEnter animation <Actors>

--- src/components/Actors/Actors.jsx
+++ src/components/Actors/Actors.jsx
@@ -17,6 +17,8 @@ import imgs from '../../assets/imgs';
 import { MovieList, PaginationCustom, ErrorNetwork } from '..';
 // useTheme (mui)
 import { useTheme } from '@mui/material/styles';
+// framer
+import { motion } from 'framer-motion';
 // redux
 import { useDispatch, useSelector } from 'react-redux';
 // redux actions
@@ -61,6 +63,16 @@ export default function Actors() {
   }, [error, navigate]);
 
   // local variables
+  // framer
+  const containerAnimation = {
+    hidden: {
+      y: '100px',
+    },
+    visible: {
+      y: '0px',
+      transition: { duration: 0.5, ease: 'easeOut' },
+    },
+  };
   // functions
   const splitText = (text) => {
     return text.split('\n').filter((part) => part.trim() !== '');
@@ -217,185 +229,193 @@ export default function Actors() {
   // primary return
   return (
     <>
-      <Container
-        maxWidth='xxl'
-        sx={{ padding: { xs: '12px', md: '32px 12px 12px 12px' } }}
+      <motion.div
+        initial='hidden'
+        animate='visible'
+        variants={containerAnimation}
       >
-        <Grid container sx={{ marginBottom: '48px' }}>
-          {/* image and text */}
-          <Grid
-            item
-            xs={12}
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'flex-start',
-              marginBottom: { xs: '32px', lg: '40px' },
-            }}
-          >
-            {/* image */}
+        <Container
+          maxWidth='xxl'
+          sx={{ padding: { xs: '12px', md: '32px 12px 12px 12px' } }}
+        >
+          <Grid container sx={{ marginBottom: '48px' }}>
+            {/* image and text */}
             <Grid
               item
               xs={12}
               sx={{
                 display: 'flex',
-                alignSelf: 'center',
                 flexDirection: 'column',
-                marginBottom: '24px',
+                alignItems: 'flex-start',
+                marginBottom: { xs: '32px', lg: '40px' },
               }}
             >
-              <Box sx={{ marginBottom: '16px', position: 'relative' }}>
-                <img
-                  alt={actor?.name}
-                  src={
-                    actor?.profile_path
-                      ? `https://image.tmdb.org/t/p/w780/${actor.profile_path}`
-                      : imgs.defaultActorImage
-                  }
-                  style={{
-                    borderRadius: '12px',
-                  }}
-                  className='Actors__cover'
-                />
-              </Box>
-            </Grid>
-
-            {/* text and imdb */}
-
-            <Grid item xs={12}>
-              {/* text */}
-              <Grid item xs={12} sx={{ marginBottom: '32px' }}>
-                {/* name */}
-                <Typography
-                  variant='h4'
-                  sx={{
-                    textAlign: 'center',
-                    fontWeight: 'bold',
-                    marginBottom: '6px',
-                    color: actor?.name
-                      ? 'inherit'
-                      : theme.palette.text.disabled,
-                  }}
-                >
-                  {actor?.name || 'No name'}
-                </Typography>
-
-                {/* bio */}
-                <Typography
-                  variant='h6'
-                  sx={{
-                    fontWeight: 'normal',
-                    color: actor?.biography
-                      ? 'inherit'
-                      : theme.palette.text.disabled,
-                  }}
-                >
-                  {actor?.biography
-                    ? splitText(actor.biography).map((paragraph, index) => (
-                        <p key={index}>{paragraph}</p>
-                      ))
-                    : 'No biography'}
-                </Typography>
-              </Grid>
-
-              {/* Details */}
+              {/* image */}
               <Grid
                 item
                 xs={12}
-                sx={{ marginBottom: '32px', alignSelf: 'flex-start' }}
+                sx={{
+                  display: 'flex',
+                  alignSelf: 'center',
+                  flexDirection: 'column',
+                  marginBottom: '24px',
+                }}
               >
-                <Typography variant='h5' sx={{ marginBottom: '12px' }}>
-                  Details
-                </Typography>
+                <Box sx={{ marginBottom: '16px', position: 'relative' }}>
+                  <img
+                    alt={actor?.name}
+                    src={
+                      actor?.profile_path
+                        ? `https://image.tmdb.org/t/p/w780/${actor.profile_path}`
+                        : imgs.defaultActorImage
+                    }
+                    style={{
+                      borderRadius: '12px',
+                    }}
+                    className='Actors__cover'
+                  />
+                </Box>
+              </Grid>
 
-                {/* imdb */}
-                {actor?.imdb_id && (
-                  <Box
+              {/* text and imdb */}
+
+              <Grid item xs={12}>
+                {/* text */}
+                <Grid item xs={12} sx={{ marginBottom: '32px' }}>
+                  {/* name */}
+                  <Typography
+                    variant='h4'
                     sx={{
-                      display: 'flex',
-                      gap: '12px',
-                      flexWrap: 'wrap',
+                      textAlign: 'center',
+                      fontWeight: 'bold',
+                      marginBottom: '6px',
+                      color: actor?.name
+                        ? 'inherit'
+                        : theme.palette.text.disabled,
                     }}
                   >
-                    <Button
-                      href={
-                        `https://www.imdb.com/name/${actor?.imdb_id}` || '#'
-                      }
-                      target='_blank'
-                      rel='noopener noreferrer'
-                      variant='outlined'
-                      endIcon={<MovieIcon />}
+                    {actor?.name || 'No name'}
+                  </Typography>
+
+                  {/* bio */}
+                  <Typography
+                    variant='h6'
+                    sx={{
+                      fontWeight: 'normal',
+                      color: actor?.biography
+                        ? 'inherit'
+                        : theme.palette.text.disabled,
+                    }}
+                  >
+                    {actor?.biography
+                      ? splitText(actor.biography).map((paragraph, index) => (
+                          <p key={index}>{paragraph}</p>
+                        ))
+                      : 'No biography'}
+                  </Typography>
+                </Grid>
+
+                {/* Details */}
+                <Grid
+                  item
+                  xs={12}
+                  sx={{ marginBottom: '32px', alignSelf: 'flex-start' }}
+                >
+                  <Typography variant='h5' sx={{ marginBottom: '12px' }}>
+                    Details
+                  </Typography>
+
+                  {/* imdb */}
+                  {actor?.imdb_id && (
+                    <Box
                       sx={{
-                        color: theme.palette.text.primary,
-                        borderColor: theme.palette.text.primary,
-                        '&:hover': { borderColor: theme.palette.text.primary },
+                        display: 'flex',
+                        gap: '12px',
+                        flexWrap: 'wrap',
                       }}
                     >
-                      IMDB
-                    </Button>
-                  </Box>
-                )}
+                      <Button
+                        href={
+                          `https://www.imdb.com/name/${actor?.imdb_id}` || '#'
+                        }
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        variant='outlined'
+                        endIcon={<MovieIcon />}
+                        sx={{
+                          color: theme.palette.text.primary,
+                          borderColor: theme.palette.text.primary,
+                          '&:hover': {
+                            borderColor: theme.palette.text.primary,
+                          },
+                        }}
+                      >
+                        IMDB
+                      </Button>
+                    </Box>
+                  )}
+                </Grid>
               </Grid>
+            </Grid>
+
+            {/* You may also like */}
+            <Grid item xs={12}>
+              <Typography variant='h5' sx={{ marginBottom: '12px' }}>
+                Movies
+              </Typography>
+              {actorMovies.results.length > 0 ? (
+                <MovieList movies={actorMovies?.results.slice(0, 12)} />
+              ) : (
+                <Typography
+                  variant='body1'
+                  sx={{ color: theme.palette.text.disabled }}
+                >
+                  No movies
+                </Typography>
+              )}
             </Grid>
           </Grid>
 
-          {/* You may also like */}
-          <Grid item xs={12}>
-            <Typography variant='h5' sx={{ marginBottom: '12px' }}>
-              Movies
-            </Typography>
-            {actorMovies.results.length > 0 ? (
-              <MovieList movies={actorMovies?.results.slice(0, 12)} />
-            ) : (
-              <Typography
-                variant='body1'
-                sx={{ color: theme.palette.text.disabled }}
-              >
-                No movies
-              </Typography>
-            )}
-          </Grid>
-        </Grid>
-
-        {/* back button */}
-        <Button
-          onClick={handleGoBack}
-          sx={{
-            position: 'absolute',
-            top: '100px',
-            right: '16px',
-            color: theme.palette.text.primary,
-            borderColor: theme.palette.text.primary,
-            textTransform: 'capitalize',
-            '&:hover': {
-              borderColor: theme.palette.text.primary,
-              bgcolor: 'transparent',
-            },
-          }}
-          variant='outlined'
-          endIcon={<ArrowBackRounded />}
-        >
-          Back
-        </Button>
-
-        {/* pagination */}
-        {actorMovies?.total_pages > 1 && (
-          <Box
+          {/* back button */}
+          <Button
+            onClick={handleGoBack}
             sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginBottom: '48px',
+              position: 'absolute',
+              top: '100px',
+              right: '16px',
+              color: theme.palette.text.primary,
+              borderColor: theme.palette.text.primary,
+              textTransform: 'capitalize',
+              '&:hover': {
+                borderColor: theme.palette.text.primary,
+                bgcolor: 'transparent',
+              },
             }}
+            variant='outlined'
+            endIcon={<ArrowBackRounded />}
           >
-            <PaginationCustom
-              currentPage={page}
-              setPage={(page) => dispatch(setPage(page))}
-              totalPages={actorMovies?.total_pages}
-            />
-          </Box>
-        )}
-      </Container>
+            Back
+          </Button>
+
+          {/* pagination */}
+          {actorMovies?.total_pages > 1 && (
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginBottom: '48px',
+              }}
+            >
+              <PaginationCustom
+                currentPage={page}
+                setPage={(page) => dispatch(setPage(page))}
+                totalPages={actorMovies?.total_pages}
+              />
+            </Box>
+          )}
+        </Container>
+      </motion.div>
     </>
   );
 }
